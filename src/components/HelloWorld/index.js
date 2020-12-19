028f81458401c28f0e339dd59a3db3ac8a16dee1
Oleh Yashchyshyn <oleh.yashchyshyn@gmail.com>
2020-12-19 13:22:07 +0200

    clear components

--- src/components/HelloWorld/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
-
-ReactDOM.render(
-    <HelloWorld />,
-    document.getElementById('root')
-);
--- src/components/IntroducingJsx/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import IntroducingJsx from './components/IntroducingJsx';
-
-ReactDOM.render(
-    <IntroducingJsx />,
-    document.getElementById('root')
-);
